c0843020a75052425f8d5f092108c3ac30522141
Akhbarona <akhbarona@yahoo.com>
2020-05-10 10:10:41 +0700

    Menambahkan file  koneksiDB.java untuk menghubungkan ke local database

---
+++ src/koneksi/koneksiDB.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package koneksi;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author USER
+ */
+public class koneksiDB {
+    private static Connection myKoneksiDB;
+    
+    public static Connection myDB()throws SQLException{
+        try{
+            String url = "jdbc:mysql://localhost:3306/penjualanmotor";
+            String user = "root";
+            String pass = "";
+            
+            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+            myKoneksiDB = DriverManager.getConnection(url, user, pass);
+        }catch(SQLException e){
+            System.out.println("Koneksi ke database gagal"+e.getMessage());
+        }
+        return myKoneksiDB;
+    }
+}
